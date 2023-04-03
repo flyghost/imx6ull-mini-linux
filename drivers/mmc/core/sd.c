8cf05562134d8c648682ca79767764802c5231b3
creekwater <zhaoyang_wu@foxmail.com>
2023-04-03 16:17:04 +0800

    修改中午标点符号

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -36,15 +36,15 @@
 
  * 这里
  * 
- * tran_exp ：传输速率单元（对应的值做了除以10的处理，原因见下面的注释）
+ * tran_exp :传输速率单元(对应的值做了除以10的处理,原因见下面的注释)
  * 		0: 100K bit/s
  * 		1: 1M   bit/s
  * 		2: 10M  bit/s
  * 		3: 100M bit/s
  * 
- * tran_mant ：时间值(由于将浮点数扩大十倍转为整形，所以对应的tran_exp对应的传输速率单元需要除以10)
- * 		0：保留
- * 		1：1.0
+ * tran_mant :时间值(由于将浮点数扩大十倍转为整形,所以对应的tran_exp对应的传输速率单元需要除以10)
+ * 		0:保留
+ * 		1:1.0
  * 		2: 1.2
  * 		3: 1.3
  * 		4: 1.5
@@ -66,14 +66,14 @@ static const unsigned int tran_exp[] = {
 	0,		0,		0,		0
 };
 
-// 最大传输速率：传输速率单元
+// 最大传输速率:传输速率单元
 static const unsigned char tran_mant[] = {
 	0,	10,	12,	13,	15,	20,	25,	30,
 	35,	40,	45,	50,	55,	60,	70,	80,
 };
 
 // 用于计算SD卡的访问时间TAAC
-// TAAC 是一个 8 位字段，其中高三位表示时间单位的指数，低三位表示时间单位的尾数
+// TAAC 是一个 8 位字段,其中高三位表示时间单位的指数,低三位表示时间单位的尾数
 // tacc_exp 数组用于查找时间单位的指数
 static const unsigned int tacc_exp[] = {
 	1,	10,	100,	1000,	10000,	100000,	1000000, 10000000,
@@ -92,7 +92,7 @@ static const unsigned int sd_au_size[] = {
 	SZ_16M / 512,	(SZ_16M + SZ_8M) / 512,	SZ_32M / 512,	SZ_64M / 512,
 };
 
-// 从resp变量中，提取从start位开始的size位，并将其作为一个无符号32位整数返回
+// 从resp变量中,提取从start位开始的size位,并将其作为一个无符号32位整数返回
 #define UNSTUFF_BITS(resp,start,size)					\
 	({								\
 		const int __size = size;				\
@@ -112,7 +112,7 @@ static const unsigned int sd_au_size[] = {
  */
 
 /**
- * @brief 根据原始的CID，解码成我们自己的CID结构体
+ * @brief 根据原始的CID,解码成我们自己的CID结构体
  * 
  * @param card mmc卡结构体
  */
@@ -133,11 +133,11 @@ void mmc_decode_cid(struct mmc_card *card)
 	card->cid.prod_name[2]		= UNSTUFF_BITS(resp, 80, 8);
 	card->cid.prod_name[3]		= UNSTUFF_BITS(resp, 72, 8);
 	card->cid.prod_name[4]		= UNSTUFF_BITS(resp, 64, 8);
-	card->cid.hwrev			= UNSTUFF_BITS(resp, 60, 4);		// 产品版本号：硬件版本号
-	card->cid.fwrev			= UNSTUFF_BITS(resp, 56, 4);		// 产品版本号：固件版本号
+	card->cid.hwrev			= UNSTUFF_BITS(resp, 60, 4);		// 产品版本号:硬件版本号
+	card->cid.fwrev			= UNSTUFF_BITS(resp, 56, 4);		// 产品版本号:固件版本号
 	card->cid.serial		= UNSTUFF_BITS(resp, 24, 32);		// 产品序列号
-	card->cid.year			= UNSTUFF_BITS(resp, 12, 8);		// 生产日期：年
-	card->cid.month			= UNSTUFF_BITS(resp, 8, 4);		// 生产日期：月
+	card->cid.year			= UNSTUFF_BITS(resp, 12, 8);		// 生产日期:年
+	card->cid.month			= UNSTUFF_BITS(resp, 8, 4);		// 生产日期:月
 
 	card->cid.year += 2000; /* SD cards year offset */
 }
@@ -147,7 +147,7 @@ void mmc_decode_cid(struct mmc_card *card)
  */
 
 /**
- * @brief 根据给定的128bit的响应，解码赋值给卡的CSD结构体
+ * @brief 根据给定的128bit的响应,解码赋值给卡的CSD结构体
  * 
  * @param card 
  * @return int 成功则返回0
@@ -163,7 +163,7 @@ static int mmc_decode_csd(struct mmc_card *card)
 	switch (csd_struct) {
 	case 0:
 		// 计算数据读访问时间TAAC
-		// BITS[112, 119]固定为0xE，代表1ms
+		// BITS[112, 119]固定为0xE,代表1ms
 		m = UNSTUFF_BITS(resp, 115, 4);					// 指数
 		e = UNSTUFF_BITS(resp, 112, 3);					// 尾数
 		csd->tacc_ns	 = (tacc_exp[e] * tacc_mant[m] + 9) / 10;
@@ -245,7 +245,7 @@ static int mmc_decode_csd(struct mmc_card *card)
  */
 
 /**
- * @brief 解码 SD 卡的 SCR（SD 卡配置寄存器）
+ * @brief 解码 SD 卡的 SCR(SD 卡配置寄存器)
  * 
  * @param card 
  * @return int 
